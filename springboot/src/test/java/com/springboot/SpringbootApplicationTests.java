1f8517d4f322bcf60b29373950f08527e99bc401
乔兆年 <1589690721@qq.com>
2020-08-10 14:59:47 +0800

    Druid JDBC

--- springboot/src/test/java/com/springboot/SpringbootApplicationTests.java
+++ springboot/src/test/java/com/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,27 @@
 package com.springboot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootTest
 class SpringbootApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
     @Test
-    void contextLoads() {
+    void contextLoads() throws SQLException {
+        //查看默认数据源
+        System.out.println(dataSource.getClass());
+        //获取数据连接
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        //关闭
+        connection.close();
     }
 
 }
