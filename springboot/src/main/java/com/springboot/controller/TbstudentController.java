a81a0db4c358aba3917ad87138ebafad73c0ea6d
乔兆年 <1589690721@qq.com>
2020-09-14 09:15:59 +0800

    知识点

--- springboot/src/main/java/com/springboot/controller/TbstudentController.java
+++ springboot/src/main/java/com/springboot/controller/TbstudentController.java
@@ -1,12 +1,16 @@
 package com.springboot.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.springboot.pojo.Tbstudent;
 import com.springboot.service.TbstudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -23,4 +27,26 @@ public class TbstudentController {
         }
         return studentList;
     }
+
+    /**
+     * 使用pagehelper进行分页，并封装进map
+     * @param page
+     * @param size
+     * @return
+     */
+    @RequestMapping("/list2")
+    public Object list(int page,int size){
+
+        PageHelper.startPage(page,size);
+        List<Tbstudent> studentList = tbstudentService.selectAll();
+        PageInfo<Tbstudent> pageInfo = new PageInfo<>(studentList);
+        Map<String,Object> data = new HashMap<>();
+        data.put("total_size",pageInfo.getTotal());
+        data.put("total_page",pageInfo.getPages());
+        data.put("current_page",page);
+        data.put("data",pageInfo.getList());
+        return data;
+    }
+
+
 }
